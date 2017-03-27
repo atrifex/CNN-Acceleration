0d7a89528d4d370cc698de55b5087197f95581cd
Atrifex <rishirt.us@gmail.com>
2017-03-27 16:19:51 -0500

    CMakeList.txt is correct. Can start porting over cuda code

--- openCL_implementation/src/config.cmake
+++ openCL_implementation/src/config.cmake
@@ -10,3 +10,5 @@ set(
   ${CMAKE_CURRENT_LIST_DIR}/utils.hpp
   ${CMAKE_CURRENT_LIST_DIR}/range.hpp
 )
+
+configure_file(${CMAKE_CURRENT_LIST_DIR}/main.cl ${CMAKE_CURRENT_BINARY_DIR}/main.cl COPYONLY)
